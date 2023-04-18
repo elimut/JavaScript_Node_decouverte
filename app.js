972823dd0111f8420f5abcddae3c7be57e98129c
elimut <121231529+elimut@users.noreply.github.com>
2023-04-18 08:18:46 +0200

    node
    post,delete, update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
-const bodyParser = require('body-parser');
 const myConnection = require('express-myconnection');
 const port = 3000;
 const optionBDD = {
@@ -11,14 +10,11 @@ const optionBDD = {
   port: 3306,
   database: 'todolist'
 }
-
 app.use(myConnection(mysql, optionBDD, 'pool'));
 app.use(express.static('public')); //definition des resources static
-app.set('views', './IHM'); //definition du chemin de mes views
+app.set('views', './IHM'); //definition du chemin de mes views dossier IHM
 app.set('view engine', 'ejs'); // definition du moteur de views
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
+app.use(express.urlencoded({extended : false}));
 app.get('/', (req, res)=>{
   req.getConnection((error, connection)=>{
     if (error) {
@@ -33,16 +29,53 @@ app.get('/', (req, res)=>{
       })
     }
   })
+})
+app.post('/liste', (req, res)=>{
+  let id = req.body.id === "" ? null : req.body.id;
+  let title = req.body.title;
+  let description = req.body.description;
+  let requeteSQL = id === null ? 'INSERT INTO liste(title, description) VALUES (?,?)'
+  : 'UPDATE liste SET title = ?, description = ? WHERE id = ?';
+  let data = id === null ? [title, description] : [title, description, id]
+  req.getConnection((error, connection)=>{
+    if (error) {
+      console.error(error);
+    } else {
+      connection.query(
+        requeteSQL,
+        data,
+        (error, data)=>{
+        if (error) {
+          console.error(error);
+        } else {
+          res.status(302).redirect('/');
+        }
+      })
+    }
+  })
 });
-
+app.delete('/liste/:id',(req, res)=>{ //les : binder + dinamyque
+  let id = req.params.id;
+  req.getConnection((error, connection)=>{
+  if (error) {
+    console.error(error);
+  }else{
+    connection.query('DELETE FROM liste WHERE id = ?', [id], (error, data)=>{
+      if (error) {
+        console.error(error);
+      }else{
+        res.status(200).json({routeRacine : '/'})
+      }
+      })
+    }
+  })
+})
 app.get('/a-propos', (req, res)=>{
   res.status(200).render('apropos')
 });
-
 app.use((req, res)=>{
   res.status(404).render('pageintrouvable')
 });
-
 app.listen(port, ()=>{
-  console.log("Server listening on port " + port);
+  console.log("Server listening on port " + port);//server port 3000
 });
